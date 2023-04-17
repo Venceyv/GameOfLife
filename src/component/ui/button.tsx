7ba42d30680e71cad36d8c6893deb67eebb5da13
Ming <steammingliu1@gmail.com>
2023-04-17 15:18:31 -0700

    fix: btn bar hover color

--- src/component/ui/button.tsx
+++ src/component/ui/button.tsx
@@ -8,7 +8,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'font-rubik-pixel bg-primary-unfilled-color text-white dark:hover:bg-primary-filled-color dark:text-white',
+          'font-rubik-pixel bg-primary-unfilled-color text-white dark:hover:bg-primary-hover-color dark:text-white',
         destructive: 'bg-red-500 text-white hover:bg-red-600 dark:hover:bg-red-600',
         outline:
           'bg-transparent border border-slate-200 hover:bg-slate-100 dark:border-slate-700 dark:text-slate-100',
